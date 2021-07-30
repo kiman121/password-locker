25738e0e447bde616a57e1779b9643ba56d235ca
Patrick Kimanthi <kimanthip@gmail.com>
2021-07-30 18:26:36 +0300

    add the save_credentials method

--- credentials.py
+++ credentials.py
@@ -2,6 +2,7 @@ class Credentials:
     '''
     Class generates new instances of credentials
     '''
+    credentials_list = []
     def __init__(self, user_id, site_name, username, password):
         '''
         __init__ method helps in defining properties for the user object.
@@ -15,3 +16,9 @@ class Credentials:
         self.site_name = site_name
         self.username = username
         self.password = password
+    
+    def save_credentials(self):
+        '''
+        save_credentials method saves credentials objects into credentials list
+        '''
+        Credentials.credentials_list.append(self)
